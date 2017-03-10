865468a0b60aae51c32bfbcffbc1aa6f4e0df5fd
xiandean <496560094@qq.com>
2017-03-10 18:00:53 +0800

    add project
    我的博客项目

---
+++ routes/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: '主页' });
+});
+
+router.get('/reg', function(req, res, next) {
+	res.render('reg', { title: '注册'});
+});
+
+router.post('/reg', function(req, res, next) {
+
+});
+
+router.get('/login', function(req, res, next) {
+	res.render('login', { title: '登陆'});
+});
+
+router.post('/login', function(req, res, next) {
+
+});
+
+router.get('/post', function(req, res, next) {
+	res.render('post', { title: '发表'});
+});
+
+router.post('/post', function(req, res, next) {
+
+});
+
+router.get('/logout', function(req, res, next) {
+
+});
+module.exports = router;
